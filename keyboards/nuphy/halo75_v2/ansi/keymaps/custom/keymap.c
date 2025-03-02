12e7f4e16944e6c8a014384ff4e4b0a5eea4dc32
AmitMaish <83043520+AmitMaish@users.noreply.github.com>
2025-03-01 16:14:53 -0800

    changed default layer to layer 2 because that's the only one with nkro

--- keyboards/nuphy/halo75_v2/ansi/keymaps/custom/keymap.c
+++ keyboards/nuphy/halo75_v2/ansi/keymaps/custom/keymap.c
@@ -14,14 +14,14 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-// layer Mac
+// layer win
 [0] = LAYOUT(
-	KC_ESC,		KC_SCRL,	KC_PAUSE,	MAC_TASK,	MAC_SEARCH, MAC_VOICE,	MAC_DND,	KC_MPRV,	KC_MPLY,	KC_MNXT,	KC_MUTE,	KC_VOLD,	KC_VOLU,	KC_DEL,		TG(5),		KC_INS,
+	KC_ESC,		KC_F1,		KC_F2,		KC_F3,		KC_F4,		KC_F5,		KC_F6,		KC_F7,		KC_F8,		KC_F9,		KC_F10,		KC_F11,		KC_F12,		KC_DEL,		_______,	KC_INS,
 	KC_GRV,		KC_1,		KC_2,		KC_3,		KC_4,		KC_5,		KC_6,		KC_7,		KC_8,		KC_9,		KC_0,		KC_MINS,	KC_EQL,		KC_BSPC,				KC_HOME,
 	KC_TAB,		KC_Q,		KC_W,		KC_E,		KC_R,		KC_T,		KC_Y,		KC_U,		KC_I,		KC_O,		KC_P,		KC_LBRC,	KC_RBRC,	KC_BSLS,				KC_END,
-	KC_ESC,		HOME_A,		HOME_S,		HOME_D,		HOME_F,		KC_G,		KC_H,		HOME_J,		HOME_K,		HOME_L,		HOME_SCLN,	KC_QUOT,	KC_ENT,								KC_PGUP,
+	KC_CAPS,	KC_A,		KC_S,		KC_D,		KC_F,		KC_G,		KC_H,		KC_J,		KC_K,		KC_L,		KC_SCLN,	KC_QUOT,	KC_ENT,								KC_PGUP,
 	KC_LSFT,				KC_Z,		KC_X,		KC_C,		KC_V,		KC_B,		KC_N,		KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	KC_RSFT,				KC_UP,		KC_PGDN,
-	KC_LCTL,	KC_LOPT,	KC_LCMD,										KC_SPC,											KC_RCMD,	MO(1),					KC_LEFT,	KC_DOWN,	KC_RIGHT),
+	KC_LCTL,	KC_LALT,	KC_LCMD,										KC_SPC,											KC_RALT,	MO(3),					KC_LEFT,	KC_DOWN,	KC_RIGHT),
 // layer Mac Fn
 [1] = LAYOUT(
 	_______,	KC_F1,		KC_F2,		KC_F3,		KC_F4,		KC_F5,		KC_F6,		KC_F7,		KC_F8,		KC_F9,		KC_F10,		KC_F11,		KC_F12,		_______,	_______,	_______,
@@ -30,15 +30,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,							_______,
 	_______,				_______,	_______,	_______,	_______,	_______,	_______,	MO(4),		RGB_SPD,	RGB_SPI,	_______,	_______,				RGB_VAI,	_______,
 	_______,	_______,	_______,										_______,										_______,	MO(1),					RGB_MOD,	RGB_VAD,	RGB_HUI),
-// layer win
+// layer win Fn
+// default layer
 [2] = LAYOUT(
-	KC_ESC,		KC_F1,		KC_F2,		KC_F3,		KC_F4,		KC_F5,		KC_F6,		KC_F7,		KC_F8,		KC_F9,		KC_F10,		KC_F11,		KC_F12,		KC_DEL,		_______,	KC_INS,
+	KC_ESC,		KC_SCRL,	KC_PAUSE,	MAC_TASK,	MAC_SEARCH, MAC_VOICE,	MAC_DND,	KC_MPRV,	KC_MPLY,	KC_MNXT,	KC_MUTE,	KC_VOLD,	KC_VOLU,	KC_DEL,		TG(5),		KC_INS,
 	KC_GRV,		KC_1,		KC_2,		KC_3,		KC_4,		KC_5,		KC_6,		KC_7,		KC_8,		KC_9,		KC_0,		KC_MINS,	KC_EQL,		KC_BSPC,				KC_HOME,
 	KC_TAB,		KC_Q,		KC_W,		KC_E,		KC_R,		KC_T,		KC_Y,		KC_U,		KC_I,		KC_O,		KC_P,		KC_LBRC,	KC_RBRC,	KC_BSLS,				KC_END,
-	KC_CAPS,	KC_A,		KC_S,		KC_D,		KC_F,		KC_G,		KC_H,		KC_J,		KC_K,		KC_L,		KC_SCLN,	KC_QUOT,	KC_ENT,								KC_PGUP,
+	KC_ESC,		HOME_A,		HOME_S,		HOME_D,		HOME_F,		KC_G,		KC_H,		HOME_J,		HOME_K,		HOME_L,		HOME_SCLN,	KC_QUOT,	KC_ENT,								KC_PGUP,
 	KC_LSFT,				KC_Z,		KC_X,		KC_C,		KC_V,		KC_B,		KC_N,		KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	KC_RSFT,				KC_UP,		KC_PGDN,
-	KC_LCTL,	KC_LALT,	KC_LCMD,										KC_SPC,											KC_RALT,	MO(3),					KC_LEFT,	KC_DOWN,	KC_RIGHT),
-// layer win Fn
+	KC_LCTL,	KC_LOPT,	KC_LCMD,										KC_SPC,											KC_RCMD,	MO(1),					KC_LEFT,	KC_DOWN,	KC_RIGHT),
 [3] = LAYOUT(
 	_______,	KC_BRID,	KC_BRIU,	KC_F3,		KC_F4,		KC_F5,		KC_F6,		KC_MPRV,	KC_MPLY,	KC_MNXT,	KC_MUTE,	KC_VOLD,	KC_VOLU,	KC_PSCR,	_______,	_______,
 	_______,	LNK_BLE1,	LNK_BLE2,	LNK_BLE3,	LNK_RF,		_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,				_______,
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,							_______,
 	_______,				_______,	_______,	_______,	_______,	_______,	_______,	_______,	SIDE_SPD,	SIDE_SPI,	SIDE_MOD_B, _______,				SIDE_VAI,	_______,
 	_______,	_______,	_______,										_______,										_______,	MO(4),					SIDE_MOD_A, SIDE_VAD,	SIDE_HUI),
-// layer default behavior
+// no homerow mods
 [5] = LAYOUT(
 	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,
 	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,				_______,
